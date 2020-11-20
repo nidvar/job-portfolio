1fe931a8d4e9921241edeebe0d4c370a480daa36
Nidvar <djayshine87@hotmail.com>
2020-11-21 10:12:21 +1100

    first commit

---
+++ src/components/images.jsx
@@ -0,0 +1,13 @@
+import chatroom from '../images/chatroom.png'
+import todo_list from '../images/todo_list.png'
+import games from '../images/games.png'
+import weather from '../images/weather.png'
+import search from '../images/search.png'
+import typingtest from '../images/typingtest.png'
+import youtube from '../images/youtube.png'
+import recipe from '../images/recipe.png'
+import scrap from '../images/scrap_icon.png'
+
+const my_images = [chatroom, todo_list, games, weather, search, typingtest, youtube, recipe, scrap]
+
+export default my_images;
